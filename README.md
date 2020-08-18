32bfb1b7e4f909a436955d59435654281388b5c8
Matt Loberg <loberg.matt@gmail.com>
2020-08-18 12:10:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mloberg/preset-eslint
+
+[use-preset](https://usepreset.dev/) preset for [eslint](https://eslint.org/).
+
+<pre align="center">npx use-preset mloberg/preset-eslint</pre>
+
+If you're using TypeScript
+
+<pre align="center">npx use-preset mloberg/preset-eslint --typescript</pre>
---
+++ preset.js
@@ -0,0 +1,43 @@
+const { Preset } = require('use-preset');
+
+module.exports = Preset.make('preset-eslint')
+    .option('typescript', false)
+
+    .editJson('package.json')
+        .merge({
+            devDependencies: {
+                'eslint': '^7.7.0',
+                'eslint-config-prettier': '^6.11.0',
+                'eslint-plugin-prettier': '^3.1.4',
+                'eslint-plugin-simple-import-sort': '^5.0.3',
+                'prettier': '^2.0.5',
+            },
+        })
+        .chain()
+
+    .editJson('package.json')
+        .merge({
+            scripts: {
+                lint: 'eslint . --ext .js',
+                fix: 'npm run lint -- --fix',
+            },
+            devDependencies: {
+                '@typescript-eslint/eslint-plugin': '^3.9.0',
+                '@typescript-eslint/parser': '^3.9.0',
+            },
+        })
+        .if(({ flags }) => Boolean(flags.typescript))
+        .chain()
+
+    .copyDirectory('default')
+        .to('/')
+        .whenConflict('override')
+        .chain()
+
+    .copyDirectory('typescript')
+        .to('/')
+        .if(({ flags }) => Boolean(flags.typescript))
+        .whenConflict('override')
+        .chain()
+
+    .installDependencies();
